250f7025546753ec31b6e154c6567c25f48ab140
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2026-01-29 03:31:37 +0900

    [setting] SwiftUI 연습 프로젝트 초기 세팅

---
+++ Practice_01/Practice_01/App/Practice_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_01App.swift
+//  Practice_01
+//
+//  Created by 조영서 on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Practice_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
